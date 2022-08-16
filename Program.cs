8be6f41a48429c49c2a6aaaf1a179828c51b6952
mani1 <mani1@NANI143>
2022-08-16 16:33:45 +0530

    [Rajeswari]Added  Email Part Has Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Registration_Regex;
+using RegexEmail;
+
+namespace RegexEmail
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Registraion problem Using Regex");
+            //To Validate Mail
+
+            RegexPattern Rp2 = new RegexPattern();
+            var result2 = Rp2.ValidateMail();
+            Console.WriteLine(result2);
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexEmail
+{
+    public class RegexPattern
+    {
+
+        /// <summary>
+        ///  E.g. abc.xyz@bl.co.in 
+        ///  Email has 3 mandatory parts (abc, bl & co)
+        ///  2 optional(xyz & in) 
+        ///  with precise @ and.positions
+        /// </summary>
+
+        string Email = "^[0-9a-zA-Z]+.[a-zA-Z]{0,5}+@[a-zA-Z]{2,3}]+.[a-zA-Z]{2,3}+.[a-zA-Z]{2,4}&";
+        string Mail = Console.ReadLine();  
+        public bool ValidateMail()
+        {
+            return Regex.IsMatch(Mail, Email);
+        }
+    }
+}
