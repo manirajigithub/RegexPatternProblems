a5569a3f84c167a125de84df3b8dc243201a558f
mani1 <mani1@NANI143>
2022-08-16 16:23:35 +0530

    [Rajeswari]Added Last Name Valid

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexLastName
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Registraion problem Using Regex");
+
+
+            //To Validate Last Name
+
+            RegexPattern Rp1 = new RegexPattern();
+            var result1 = Rp1.ValidateLast();
+            Console.WriteLine(result1);
+        }
+    }
+}
+
---
+++ RegexPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexLastName
+{
+    public class RegexPattern
+    {
+        
+        /// <summary>
+        /// Name starts with Capital
+        /// Length Min 3 Char
+        /// </summary>
+        string LastName = "^[A-Z]{1}[a-zA-Z]{3,15}$";
+        string Last = Console.ReadLine();  
+        public bool ValidateLast()
+        {
+            return Regex.IsMatch(Last, LastName);
+        }
+
+    }
+}
