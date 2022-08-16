c544b29c2c5cb6dce002f3a610a0187547c92734
mani1 <mani1@NANI143>
2022-08-16 16:02:27 +0530

    [Rajeswari]Added Valid First Name Regex Pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexFirstName
+{
+    class program
+    {
+        public static void Main(string[] args)
+            
+        {
+            //To Validate First Name
+
+
+            RegexPattern Rp = new RegexPattern();
+            var result = Rp.Validate();
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ RegexPattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexFirstName
+{
+    public class RegexPattern
+    {
+        /// <summary>
+        /// Name starts with Capital
+        /// Length Min 3 Char
+        /// </summary>
+        string FirstName = "^[A-Z]{1}[a-zA-Z]{3,10}$";
+        string First = Console.ReadLine();
+        public bool Validate()
+        {
+            return Regex.IsMatch(First, FirstName);
+        }
+
+    }
+}
