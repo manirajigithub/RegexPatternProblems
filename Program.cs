4f390dae535079d6192c0c62fc8fe74698550969
mani1 <mani1@NANI143>
2022-08-17 20:43:47 +0530

    [Rajeswari]Mobile Number Part Has Done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RegexMobileNumber;
+
+namespace RegexMobileNUmber
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Registraion problem Using Regex");
+            RegexPattern Rp3 = new RegexPattern();
+            var result3 = Rp3.ValidateNumber();
+            Console.WriteLine(result3);
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ RegexPattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexMobileNumber
+{
+    public class RegexPattern
+    {
+        
+        /// <summary>
+        /// Mobile Format - E.g. 91 9919819801 
+        /// Country code followed by space and 10 digit number
+        /// </summary>
+
+        string MobileNumber = "^[91]+[]+[6-9]{1}+[0-9]{9}$";
+        string Number = Console.ReadLine();
+
+       
+        public bool ValidateNumber()
+        {
+            return Regex.IsMatch(Number, MobileNumber);
+        }
+    }
+}
