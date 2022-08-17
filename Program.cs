3552974fd79e33877ba7804a2385b189d8e3cb85
mani1 <mani1@NANI143>
2022-08-17 20:51:09 +0530

    [Rajeswari]Added Mobile Number Part

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Registration_Regex;
+
+namespace RegexPassWord
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Registraion problem Using Regex");
+
+            //To Validate Password
+
+            RegexPattern Rp4 = new RegexPattern();
+            var result4 = Rp4.ValidatePassword();
+            Console.WriteLine(result4);
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
---
+++ RegexPattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Registration_Regex
+{
+    public class RegexPattern
+    {
+       
+
+        /// <summary>
+        /// Password rules 
+        /// minimum 8 Characters
+        ///  at least 1 Upper Case 
+        ///  at least 1 numeric number
+        ///  Exactly 1 Special Character
+        /// </summary>
+        string Password = "^[A-Z]{1}+[a-zA-Z0-9]{8,}+[0-9]{1}+[!@#$%&*]{1}$";
+        string Pass = Console.ReadLine();
+        public bool ValidatePassword()
+        {
+            return Regex.IsMatch(Pass, Password);
+        }
+    }
+}
+
