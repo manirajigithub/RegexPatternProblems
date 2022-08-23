a791903ae0a2800bf2117a6d1cec96b40b6fd133
mani1 <mani1@NANI143>
2022-08-23 15:40:43 +0530

    [Rajeswari]Pasword Contains Contains Atleast one special Character

---
+++ Password4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPasswordUC8
+{
+    public class Password4
+    {
+        public static string REGEX_Password4 = "^[A-Z]{1}[A-Z a-z]{7,}[!*@#$%^&+=]?[0-9]{1,}$";
+
+        public bool ValidatePassword4(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password4);
+        }
+    }
+}
