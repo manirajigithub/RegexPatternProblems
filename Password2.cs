c802fb15ef7d898315bc8bbf0d88a042947b6c4f
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-23 15:30:11 +0530

    [Rajeswari]Added UC6

---
+++ Password2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPassWordUC6
+{
+    public class Password2
+    {
+        public static string REGEX_Password2 = "^[A-Z]{1}[A-Z a-z]{7,}$";
+
+        public bool ValidatePassword2(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password2);
+        }
+    }
+}
