c996352517ac364bbf76c9a42858e2bea9cb06f3
mani1 <mani1@NANI143>
2022-08-23 15:37:55 +0530

    [Rajeswari]Added UC8

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegexPasswordUC8
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Registration Problem");
+            /
+
+            //UC8
+            Password4 password4 = new Password4();
+            Console.WriteLine("Please Enter your password to check");
+            var check8 = password4.ValidatePassword4(Console.ReadLine());
+            Console.WriteLine(check8);
+        }
+    }
+
+}
