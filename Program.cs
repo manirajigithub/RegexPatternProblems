e79f749f0318ed4db0d71222a64730f3b7237008
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-23 15:43:27 +0530

    [Rajeswari]Added UC6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace RegexPasswordUC6
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Registration Problem");
+            
+
+            //UC6
+            Password2 password2 = new Password2();
+            Console.WriteLine("Please Enter your password to check");
+            var check6 = password2.ValidatePassword2(Console.ReadLine());
+            Console.WriteLine(check6);
+        }
+    }
+}
