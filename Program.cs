92a0b6cd8629b4cd3e4cc74a1259c807f5162ff7
mani1 <mani1@NANI143>
2022-08-23 15:48:00 +0530

    [Rajeswari]Added UC9

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RegexSampleEmails
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Registration Problem");
+            
+            //UC9
+            SampleEmails sampleEmail = new SampleEmails();
+            Console.WriteLine("Please Enter your sample emails to check");
+            var check9 = sampleEmail.ValidateEmail(Console.ReadLine());
+            Console.WriteLine(check9);
+        }
+    }
+
+}
