02d4eab3f84cba724c7c1d7615c6ad6e156d0849
mani1 <mani1@NANI143>
2022-08-23 15:34:57 +0530

    [Rajeswari]Added UC7

---
+++ Password3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPasswordUC7
+{
+    public class Password3
+    {
+        public static string REGEX_Password3 = "^[A-Z]{1}[A-Z a-z]{7,}[0-9]{1,}$"; 
+
+        public bool ValidatePassword3(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password3);
+        }
+    }
+}
