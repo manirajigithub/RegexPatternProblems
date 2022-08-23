045de4e36db3c278a0058fda32b25fab552577db
mani1 <mani1@NANI143>
2022-08-23 15:32:32 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RegexPasswordUC7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Registration Problem");
+            
+            //UC7
+            Password3 password3 = new Password3();
+            Console.WriteLine("Please Enter your password to check");
+            var check7 = password3.ValidatePassword3(Console.ReadLine());
+            Console.WriteLine(check7);
+        }
+    }
+}
