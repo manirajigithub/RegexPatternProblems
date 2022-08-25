3f91beb21f607a11ae0425fa25d510f660bf7938
mani1 <mani1@NANI143>
2022-08-25 21:29:03 +0530

    [Rajeswari]Added Invalid Exception Using Lambda

---
+++ InvalidException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblemUC13
+{
+    internal class InvalidlException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_FIRST_NAME,
+            INVALID_LAST_NAME,
+            INVALID_EMAIL,
+            INVALID_MOBILE_NUMBER,
+            INVALID_PASSWORD
+        }
+        private ExceptionType type;
+        public InvalidlException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
