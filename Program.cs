0005c8e52a79531e53e2a9f9a48b7aef87fd3ce8
mani1 <mani1@NANI143>
2022-08-25 21:22:35 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+
+namespace UserRegistrationProblemUC13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Welcome To the User Registration Problem");
+            
+
+            
+            UserRegistrationValidation validate = new UserRegistrationValidation();
+
+            Console.WriteLine("Enter First Name : ");
+            string firstName = Console.ReadLine();
+            bool fNameResult = validate.ValidateFirstName(firstName);
+            validate.PrintResult(fNameResult);
+
+
+            
+            Console.WriteLine("Enter Last Name : ");
+            string lastName = Console.ReadLine();
+            bool lNameResult = validate.ValidateLastName(lastName);
+            validate.PrintResult(lNameResult);
+
+            Console.WriteLine("Enter Email Id : ");
+            string email = Console.ReadLine();
+            bool emailResult = validate.ValidateEmail(email);
+            validate.PrintResult(emailResult);
+
+            
+            Console.WriteLine("Enter Mobile Number : ");
+            string mobNumber = Console.ReadLine();
+            bool mobNumberResult = validate.ValidateMobileNumber(mobNumber);
+            validate.PrintResult(mobNumberResult);
+
+            
+
+            Console.WriteLine("Enter Password : ");
+            string password = Console.ReadLine();
+            bool passwordResult = validate.ValidatePassword(password);
+            validate.PrintResult(passwordResult);
+
+
+            
+            Console.WriteLine("Checking for sample mails : ");
+            foreach (string mail in validate.GetList())
+            {
+                Console.Write(mail + " : ");
+                validate.PrintResult(validate.ValidateEmail2(mail));
+            }
+
+          
+
+            Console.ReadLine();
+        }
+    }
+}
